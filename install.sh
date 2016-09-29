f1c5bf0159e51e6471d6313ef4541f4048dd6c96
Patrick Hulin <patrick.hulin@gmail.com>
2016-09-28 21:08:26 -0400

    Make install.sh actually work.

--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+function link() {
+    ln -s "$dir/$1" ~
+}
+
 cd $(dirname $0)
-ln -s .tmux.conf .bashrc .gdbinit .gitconfig .tmux.conf .dircolors ~/
-pushd spf13-vim
-./bootstrap.sh
-popd
-ln -s .vimrc.before.local .vimrc.local .vimrc.bundles.local ..
+dir=$(pwd)
+link .tmux.conf
+link .bashrc
+link .gdbinit
+link .gitconfig
+link .tmux.conf
+link .dircolors
+git submodule init
+git submodule update
+if [ ! -d ~/.spf13-vim-3 ]; then
+    pushd spf13-vim
+    ./bootstrap.sh
+    popd
+fi
+rm ~/.vimrc.before.local ~/.vimrc.local ~/.vimrc.bundles.local
+link .vimrc.before.local
+link .vimrc.local
+link .vimrc.bundles.local
