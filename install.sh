99b491f975631770ebd50d8269434911479e1b81
Patrick Hulin <patrick.hulin@gmail.com>
2017-01-03 12:13:03 -0500

    First stab at install.sh for desktops.

--- install.sh
+++ install.sh
@@ -14,6 +14,12 @@ function link() {
     ln -s "$file" "$target"
 }
 
+function apt_install() {
+    if ! dpkg-query -l "$1" &> /dev/null; then
+        sudo apt-get install "$1"
+    fi
+}
+
 cd $(dirname $0)
 dir=$(pwd)
 link .tmux.conf
@@ -48,3 +54,48 @@ mkdir -p ~/bin
 link bin/up
 link bin/qemu-ctags
 link bin/doge
+
+git submodule init
+git submodule update
+if which lsb_release && ( lsb_release -d | grep -q Ubuntu ) && \
+        dpkg-query -l ubuntu-desktop &> /dev/null; then
+    # Ubuntu Desktop
+    echo "Linking in Powerline fonts & fontconfig information..."
+    mkdir -p ~/.fonts.conf.d ~/.config/fontconfig/conf.d ~/.fonts/
+    ln -s powerline/develop/font/PowerlineSymbols.otf ~/.fonts/
+    fc-cache -vf ~/.fonts/
+    ln -s powerline/develop/font/10-powerline-symbols.conf ~/.fonts.conf.d/
+    ln -s powerline/develop/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+    echo "Setting up Gnome Terminal profile..."
+    apt_install dconf-cli
+    gnome-terminal-colors-solarized/set_dark.sh
+
+    echo "Mapping Caps to Escape..."
+    dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+
+    echo "Removing icons from launcher..."
+    apt_install libglib2.0-bin
+    python - <<EOF
+import subprocess, shlex
+exec("l = " + subprocess.check_output(
+    shlex.split("gsettings get com.canonical.Unity.Launcher favorites")
+))
+l = [s for s in l if 'amazon' not in s and 'libreoffice' not in s]
+subprocess.check_call(shlex.split(
+    "gsettings set com.canonical.Unity.Launcher favorites \"%s\"" % l
+))
+EOF
+elif uname | grep -q Darwin; then
+    if [ ! -e "~/Library/Fonts/Menlo for Powerline.ttf" ]; then
+        cp Menlo-for-Powerline/*.ttf ~/Library/Fonts/
+    fi
+
+    if ! defaults read com.apple.Terminal "Window Settings" | \
+            grep -q "Solarized Dark xterm-256color"; then
+        echo "Setting up Solarized profile."
+        open "solarized/osx-terminal.app-colors-solarized/xterm-256color/Solarized Dark xterm-256color.terminal"
+        defaults write com.apple.Terminal "Startup Window Settings" "Solarized Dark xterm-256color"
+        defaults write com.apple.Terminal "Default Window Settings" "Solarized Dark xterm-256color"
+    fi
+fi
